86fcbc127e82d51564a321e91e1a3f7e72e19178
DESKTOP-8SRFDDM\cam95 <cam9548@gmail.com>
2023-01-02 20:50:07 -0600

    init mind meld

---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.resolve(__dirname, './build')));
+
+
+app.get('/api/game/:gameId', (req, res) => {
+  res.json({ message: 'hello world'});
+});
+
+// app.get('/game/:gameId', (req, res) => {
+//     res.json({ message: 'game id', gameId: req });
+//     console.log('gameid', req)
+//   });
+
+const db = {
+    test: {
+        users: [{
+            name: 'user1',
+            id: 1234,
+            words: ['apple', 'banana']
+        }, {
+            name: 'user2',
+            id: 1235,
+            words: ['plane', 'lime']
+        }],
+        isComplete: false,
+        id: 1
+    }
+};
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+    socket.on('ping', (msg) => {
+        io.emit('pong', msg)
+        console.log('pong -> ping');
+    });
+
+    socket.on('setUserId', (id) => {
+        console.log('new user', id);
+    });
+
+    socket.on('create', (gameId, userInfo) => {
+        if (db[gameId]) {
+            io.emit('error', 'Game already exists');
+        } else {
+            console.dir(db, { depth: null })
+            db[gameId] = {
+                users: [userInfo],
+                id: gameId
+            };
+            socket.join(gameId);
+            io.to(gameId).emit('currentGame', db[gameId]);
+            // socket.emit('currentGame', db[gameId])
+        }
+    })
+
+    socket.on('game', (gameId, userInfo) => {
+        if (!db[gameId]) {
+            io.emit('error', 'Game does not exist');
+        } else {
+            db[gameId].users.push(userInfo)
+            socket.emit('currentGame', db[gameId])
+            socket.join(gameId);
+            io.to(gameId).emit('currentGame', db[gameId]);
+
+            // socket.broadcast.emit('currentGame', db[gameId])
+        }
+    });
+
+    socket.on('word', ({ word, gameId, userId }) => {
+        console.log('word, gameId, userId', word, gameId, userId);
+        if (!db[gameId]) {
+            console.warn('no game found for gameId: ', gameId)
+            return;
+        }
+        const user = db[gameId]?.users.find(user => user.id === userId);
+        console.dir(user, { depth: null });
+        user.words.push(word);
+        socket.emit('currentGame', db[gameId]);
+        io.to(gameId).emit('currentGame', db[gameId]);
+    })
+
+    socket.on('disconnect', (reason) => {
+        console.log(reason, socket.id, "disconnected")
+    })
+});
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log('listening on *:', port);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useMemo, useState } from 'react';
+import io from 'socket.io-client';
+import { v4 as uuid } from 'uuid';
+
+import { Button, Form, Card, ListGroup } from 'react-bootstrap';
+
+import './App.scss';
+
+const socket = io();
+
+const userId = uuid();
 
 function App() {
+  const [currentGame, setCurrentGame] = useState();
+  const [errorMsg, setErrorMsg] = useState();
+  const [userTyping, setUserTyping] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      socket.emit('setUserId', userId)
+    });
+
+    return () => {
+      socket.off('connect');
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on('currentGame', (game) => {
+      setCurrentGame({
+        ...currentGame,
+        ...game
+      })
+    })
+
+    return () => {
+      socket.off('currentGame');
+    };
+  }, [currentGame]);
+
+  useEffect(() => {
+    socket.on('error', (error) => {
+      console.warn(error)
+      setErrorMsg(error)
+    })
+
+    return () => {
+      socket.off('error');
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentGame) {
+      const userWords = [...currentGame.users].map(user => user.words.length);
+      userWords.sort((a, b) => b - a);
+      console.log(userWords)
+      const everyWordIsIn = userWords.every(val => val === userWords[0]);
+      console.log(everyWordIsIn, userWords[0])
+      setUserTyping(!everyWordIsIn ? userWords[0] : false);
+      if (everyWordIsIn && !userWords.every(val => val === 0)) {
+        const everyFinalWord = currentGame.users.map(user => user.words[user.words.length - 1]);
+        const sameEveryWord = everyFinalWord.every(val => val === everyFinalWord[0]);
+        setGameWon(sameEveryWord);
+      }
+    }
+  }, [currentGame])
+
+  const createGame = (e) => {
+    e.preventDefault();
+    const gameId = uuid();
+    socket.emit('create', gameId, {
+      id: userId,
+      name: e.target.form.name.value,
+      words: []
+    });
+  }
+
+  const joinGame = (e) => {
+    e.preventDefault();
+    const gameId = e.target.form.code.value;
+    console.log('trying to join', gameId)
+    socket.emit('game', gameId, {
+      id: userId,
+      name: e.target.form.name.value,
+      words: []
+    });
+  }
+
+  const sendWord = (e) => {
+    e.preventDefault();
+    socket.emit('word', { word: e.target.form.word.value, gameId: currentGame.id, userId });
+    e.target.form.word.value = '';
+  }
+
+  const currentUser = useMemo(() => currentGame?.users.find(user => user.id === userId) ?? {}, [currentGame])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <h1>Mind Meld</h1>
+        <p>Try to guess the same word!</p>
       </header>
+      {gameWon && <h1>You won!</h1>}
+      {errorMsg && <div className='error'>{errorMsg}</div>}
+      {!currentGame ? (
+
+        <Intro createGame={createGame} joinGame={joinGame} />
+      ) : (
+          <div>
+            <div>current game: {currentGame.id}</div>
+            <div className="users">{currentGame.users.map(user => {
+              return (
+                <Card key={user.id} className="user">
+                  <Card.Header>{user.name}</Card.Header>
+                  <ListGroup variant="flush">
+                    {user.words.map((word, index) => {
+                      const listItem = index === userTyping - 1 ? '✔' : word;
+                      return  <ListGroup.Item className='word'>{listItem}</ListGroup.Item>
+                    })}
+                  </ListGroup>
+                </Card>
+              );
+            })}
+            </div>
+            <Form>
+              <Form.Group className="mb-3" controlId="word">
+                <Form.Label>Word:</Form.Label>
+                <Form.Control as="input" rows={3} />
+                <Button onClick={sendWord} disabled={gameWon || (currentUser.words.length > 0 && currentUser.words.length === userTyping)}>Send</Button>
+              </Form.Group>
+            </Form>
+          </div>
+      )}
     </div>
+  );
+}
+
+function Intro({ createGame, joinGame }) {
+  return (
+      <Form>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control as="input" rows={3} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="code">
+          <Form.Label>Code:</Form.Label>
+          <Form.Control as="input" rows={3} />
+        </Form.Group>
+        <Form.Group controlId="create">
+          <Button variant="primary" size="lg" onClick={joinGame}>Join game</Button>
+          <Button variant="secondary" size="lg" onClick={createGame}>Create game</Button>
+        </Form.Group>
+      </Form>
   );
 }
 
